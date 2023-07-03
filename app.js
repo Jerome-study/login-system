3fbfc1185828596be0ecf5a828c58af41e99415d
jerome.studypurpose@gmail.com <jerome.studypurpose@gmail.com>
2023-07-03 15:08:21 +0800

    update

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-app.use('/', mainRouter)
+app.use('/blog', mainRouter)
 
 
 
 
 
-const PORT =  process.env.PORT || 5000
+const PORT =  process.env.PORT 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
