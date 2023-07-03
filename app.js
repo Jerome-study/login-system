c38efe7548203d9adcadb4e49eb6d2f316feffe8
jerome.studypurpose@gmail.com <jerome.studypurpose@gmail.com>
2023-07-03 15:05:23 +0800

    delete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mainRouter = require('./routes/server.js')
+const mainRouter = require('./routes/server')
 const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
